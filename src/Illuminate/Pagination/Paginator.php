ddce8bf0250a60ff3fdee8ddb4b59aa0d987029b
Taylor Otwell <taylorotwell@gmail.com>
2012-09-12 23:29:04 -0500

    work on paginator.

--- src/Illuminate/Pagination/Paginator.php
+++ src/Illuminate/Pagination/Paginator.php
@@ -5,8 +5,16 @@ use ArrayAccess;
 use ArrayIterator;
 use IteratorAggregate;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class Paginator implements ArrayAccess, Countable, IteratorAggregate {
+
+	/**
+	 * The translator implementation.
+	 *
+	 * @var Symfony\Component\Translation\TranslatorInterface
+	 */
+	protected $trans;
 
 	/**
 	 * The request instance.
@@ -23,16 +31,99 @@ class Paginator implements ArrayAccess, Countable, IteratorAggregate {
 	protected $items;
 
 	/**
+	 * The amount of items to show per page.
+	 *
+	 * @var int
+	 */
+	protected $perPage;
+
+	/**
+	 * The locale of the paginator.
+	 *
+	 * @var string
+	 */
+	protected $locale;
+
+	/**
+	 * All of the additional query string values.
+	 *
+	 * @var array
+	 */
+	protected $query = array();
+
+	/**
 	 * Create a new Paginator instance.
 	 *
+	 * @param  Symfony\Component\Translation\TranslatorInterface  $trans
 	 * @param  Symfony\Component\HttpFoundation\Request  $request
 	 * @param  array  $items
+	 * @param  int    $perPage
 	 * @return void
 	 */
-	public function __construct(Request $request, array $items)
+	public function __construct(TranslatorInterface $trans, Request $request, array $items, $perPage)
 	{
+		$this->trans = $trans;
 		$this->items = $items;
 		$this->request = $request;
+		$this->perPage = $perPage;
+	}
+
+	/**
+	 * Setup the pagination context (current and last page).
+	 *
+	 * @return void
+	 */
+	public function setupPaginationContext()
+	{
+		$this->lastPage = ceil(count($this->items) / $this->perPage);
+
+		$this->currentPage = $this->getCurrentPage($this->lastPage);
+	}
+
+	/**
+	 * Get the current page for the request.
+	 *
+	 * @param  int  $lastPage
+	 * @return int
+	 */
+	protected function getCurrentPage($lastPage)
+	{
+		$page = $this->request->query->get('page', 1);
+
+		// The page number will get validated and adjusted if it either less than one
+		// or greater than the last page available based on the count of the given
+		// items array. If it's greater than the last, we'll give back the last.
+		if (is_numeric($page) and $page > $lastPage)
+		{
+			return $lastPage > 0 ? $lastPage : 1;
+		}
+
+		return $this->isValidPageNumber($page) ? $page : 1;
+	}
+
+	/**
+	 * Determine if the given value is a valid page number.
+	 *
+	 * @param  int  $page
+	 * @return bool
+	 */
+	protected function isValidPageNumber($page)
+	{
+		return $page >= 1 and filter_var($page, FILTER_VALIDATE_INT) !== false;
+	}
+
+	/**
+	 * Add a query string value to the paginator.
+	 *
+	 * @param  string  $key
+	 * @param  string  $value
+	 * @return Illuminate\Pagination\Paginator
+	 */
+	public function addQuery($key, $value)
+	{
+		$this->query[$key] = $value;
+
+		return $this;
 	}
 
 	/**
@@ -43,6 +134,27 @@ class Paginator implements ArrayAccess, Countable, IteratorAggregate {
 	public function getItems()
 	{
 		return $this->items;
+	}
+
+	/**
+	 * Get the locale of the paginator.
+	 *
+	 * @return string
+	 */
+	public function getLocale()
+	{
+		return $this->locale;
+	}
+
+	/**
+	 * Set the locale of the paginator.
+	 *
+	 * @param  string  $locale
+	 * @return void
+	 */
+	public function setLocale($locale)
+	{
+		$this->locale = $locale;
 	}
 
 	/**
