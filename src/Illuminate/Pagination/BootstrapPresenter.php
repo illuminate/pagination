e8279fd4456df5f0fef2ad30568e3a108b329531
Taylor Otwell <taylorotwell@gmail.com>
2012-09-16 00:22:17 -0500

    Work on paginator.

--- src/Illuminate/Pagination/BootstrapPresenter.php
+++ src/Illuminate/Pagination/BootstrapPresenter.php
@@ -160,6 +160,9 @@ class BootstrapPresenter {
 	 */
 	public function getPrevious($text = '&laquo;')
 	{
+		// If the current page is less than or equal to one, it means we can't go any
+		// further back in the pages, so we will render a disabled previous button
+		// when that is the case. Otherwise, we will give it an active "status".
 		if ($this->currentPage <= 1)
 		{
 			return '<li class="disabled"><a href="#">'.$text.'</a></li>';
@@ -180,6 +183,9 @@ class BootstrapPresenter {
 	 */
 	public function getNext($text = '&raquo;')
 	{
+		// If the current page is greater than or equal to the last page, it means we
+		// can't go any further into the pages, as we're already on this last page
+		// that is available, so we will make it the "next" link style disabled.
 		if ($this->currentPage >= $this->lastPage)
 		{
 			return '<li class="disabled"><a href="#">'.$text.'</a></li>';
