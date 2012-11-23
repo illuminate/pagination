f2f0fbd6d1ab7ed919fdb699c92e85d27b75890f
Taylor Otwell <taylorotwell@gmail.com>
2012-11-23 14:34:51 -0600

    clean up paginator.

--- src/Illuminate/Pagination/PaginationServiceProvider.php
+++ src/Illuminate/Pagination/PaginationServiceProvider.php
@@ -12,30 +12,9 @@ class PaginationServiceProvider extends ServiceProvider {
 	 */
 	public function register($app)
 	{
-		$app['pagination.driver'] = $app->share(function($app)
-		{
-			return $app['view']->driver('php');
-		});
-
-		$this->registerPaginationEnvironment($app);
-	}
-
-	/**
-	 * Register the pagination environment.
-	 *
-	 * @param  Illuminate\Foundation\Application  $app
-	 * @return void
-	 */
-	protected function registerPaginationEnvironment($app)
-	{
 		$app['paginator'] = $app->share(function($app)
 		{
-			// The pagination driver is the View driver responsible for rendering the
-			// pagination views. Typically, it will be the "PHP" driver as that is
-			// what every default pagination views use, but it could be tweaked.
-			$view = $app['pagination.driver'];
-
-			$paginator = new Environment($app['request'], $view, $app['translator']);
+			$paginator = new Environment($app['request'], $app['view'], $app['translator']);
 
 			$paginator->setViewName($app['config']['view.pagination']);
 
